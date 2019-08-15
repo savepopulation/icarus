d471476f85c930c431d8b6bfc5dbc6d892f8d681
tyln <taylan.sbrcn@gmail.com>
2019-08-15 20:48:05 +0300

    kotlin dsl implemented

---
+++ buildSrc/src/main/java/Config.kt
@@ -0,0 +1,36 @@
+/*
+ * App configuration
+ */
+object Config {
+    const val applicationId = "com.raqun.icarus"
+    const val minSdkVersion = Versions.minSdkVersion
+    const val targetSdkVersion = Versions.targetSdkVersion
+    const val compileSdkVersion = Versions.compileSdkVersion
+    const val testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    const val versionCode = 0
+    const val versionName = "1.0.0"
+}
+
+/*
+ * Flavor Dimensions
+ */
+object Dimensions {
+    const val default = "default"
+}
+
+/*
+ * Product Flavors
+ */
+object Prod {
+    const val versionCode = Config.versionCode
+    const val versionName = Config.versionName
+}
+
+object Dev {
+    const val suffix = ".dev"
+    const val versionCode = Config.versionCode * 10000
+    const val versionName = Config.versionName
+    const val versionNameSuffix = suffix
+    const val applicationIdSuffix = suffix
+}
+
---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,6 @@
+/*
+ * App Modules
+ */
+object Modules {
+    
+}

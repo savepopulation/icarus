b8ccdd8976f8293bcfb174cbedf171519a47a13c
tyln <taylan.sbrcn@gmail.com>
2020-05-28 19:35:47 +0300

    updated versions

--- buildSrc/src/main/java/Plugins.kt
+++ buildSrc/src/main/java/Plugins.kt
@@ -2,6 +2,7 @@
  * Plugins
  */
 object Plugins {
+    const val kotlin = "kotlin"
     const val androidApplication = "com.android.application"
     const val androidLibrary = "com.android.library"
     const val kotlinAndroid = "kotlin-android"
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -14,7 +14,7 @@ object Versions {
      */
     const val xVersion = "1.0.0"
     const val gradleVersion = "3.2.1"
-    const val kotlinVersion = "1.3.31"
+    const val kotlinVersion = "1.3.72"
     const val jUnitVersion = "4.12"
     const val testRunnerVersion = "1.1.0"
     const val espressoCoreVersion = "3.1.0"

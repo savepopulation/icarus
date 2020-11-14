ee4fd68ae195e153524ff3f9e3409c307153ee81
tyln <taylan.sbrcn@gmail.com>
2020-11-14 20:54:08 +0300

    lib documented

--- icarus_core/src/main/java/com/raqun/icarus/core/Icarus.kt
+++ icarus_core/src/main/java/com/raqun/icarus/core/Icarus.kt
@@ -5,38 +5,61 @@ import android.content.Intent
 import androidx.fragment.app.Fragment
 
 object Icarus {
-
+    /*
+     * Keeps the classes
+     */
     private val classes = mutableMapOf<String, Class<*>>()
 
+    /*
+     * Package name of the App to create intents
+     */
     private lateinit var packageName: String
 
+    /*
+     * Icarus should be initialized globally before using generated classes
+     */
     fun init(context: Context) {
         this.packageName = context.packageName
     }
 
+    /*
+     * Checks the library is initialized or not!
+     */
     private fun checkIcarus() {
         if (!this::packageName.isInitialized) {
             throw IllegalStateException("Icarus is not initialized!")
         }
     }
 
+    /*
+     * Returns the Intent of Dynamic Activity Feature
+     */
     private fun intentTo(className: String): Intent {
         checkIcarus()
         return Intent(Intent.ACTION_VIEW).setClassName(this.packageName, className)
     }
 
+    /*
+     * Extension function which is being called from Intent Features
+     */
     fun String.createIntentFeature() = try {
         Class.forName(this).run { intentTo(this@createIntentFeature) }
     } catch (e: ClassNotFoundException) {
         null
     }
 
+    /*
+     * Extension function which is being called from Fragment Features
+     */
     fun String.createFragmentFeature() = try {
         this.getFeature<Fragment>()?.newInstance()
     } catch (e: ClassNotFoundException) {
         null
     }
 
+    /*
+     * Gets the feature with the given class name
+     */
     private fun <T> String.getFeature(): Class<out T>? =
         classes.getOrPut(this) {
             try {

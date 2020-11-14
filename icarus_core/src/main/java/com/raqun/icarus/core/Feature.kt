05a1e1b63bf88a7d6610bb45eadea8a0a0874f07
tyln <taylan.sbrcn@gmail.com>
2020-11-14 20:47:51 +0300

    code documented

--- icarus_core/src/main/java/com/raqun/icarus/core/Feature.kt
+++ icarus_core/src/main/java/com/raqun/icarus/core/Feature.kt
@@ -1,6 +1,17 @@
 package com.raqun.icarus.core
 
+/*
+ * Defines the class is a Dynamic Feature and carries information about
+ * the path of the feature and how to create the feature dynamically
+ */
 interface Feature<T> {
+    /*
+     * Path of the Feature
+     */
     val path: String?
+
+    /*
+     * Methods that creates feature
+     */
     val dynamicStart: T?
 }
